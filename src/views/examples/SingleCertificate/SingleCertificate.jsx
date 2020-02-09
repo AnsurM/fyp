dc5450f7b7e7038021a2e38b819613ebf58d0eb6
syedahmed95 <syedahmed978@msn.com>
2020-02-09 22:34:58 +0500

    Routes connected

--- src/views/examples/SingleCertificate/SingleCertificate.jsx
+++ src/views/examples/SingleCertificate/SingleCertificate.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 // reactstrap components
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col,Card, CardTitle, CardText, CardImg, CardImgOverlay } from "reactstrap";
 
 import Header from "components/Headers/StudentHeader.jsx";
 
@@ -35,16 +35,27 @@ export default class SingleCertificate extends Component {
                 }}
               >
                 <div className="degree-container">
-                  <img
+                  {/* <img
                     className="degree-image"
                     src={require("../../../assets/img/icons/common/degree.jpg")}
-                  />
+                  /> */}
+                  <Card style={{width:"60%"}}>
+                  <CardImg width="100%" src={require("../../../assets/img/icons/common/degree.jpg")} alt="Card image cap" />
+                  <CardImgOverlay>
+                  <CardTitle>Card Title</CardTitle>
+                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                  <CardText>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                  </CardText>
+                  </CardImgOverlay>
+                  </Card>
                 </div>
               </div>
             </Col>
             {/* <Col xl="1"></Col> */}
           </Row>
         </Container>
+         
       </>
     );
   }
